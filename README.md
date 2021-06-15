6c876a3e32b907b23a5c6d01c29c616aa5eaf4a3
Lari Hotari <lari@hotari.net>
2021-06-15 11:40:06 +0300

    Add initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Pulsar Log4j2 appender demo with Spring Boot
+
+### Starting local Pulsar standalone
+
+```bash
+docker run --name pulsar-standalone -d -p 8080:8080 -p 6650:6650 apachepulsar/pulsar:2.7.2 /pulsar/bin/pulsar standalone
+```
+
+### Running application
+
+```bash
+./mvnw spring-boot:run
+```
+
+### Consuming events
+
+```bash
+docker exec -it pulsar-standalone /pulsar/bin/pulsar-client consume -s sub -p Earliest -n 0 persistent://public/default/logging-demo
+```
+
+### Producing log events
+
+Use the `http://localhost:8081/log` endpoint to create log events
+
+```bash
+uptime | curl -H 'Content-Type: text/plain' -d @- http://localhost:8081/log
+```
---
+++ src/main/java/com/github/lhotari/pulsarlogging/demo/LoggingController.java
@@ -0,0 +1,31 @@
+package com.github.lhotari.pulsarlogging.demo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoggingController {
+    private static final Logger LOG = LoggerFactory.getLogger(LoggingController.class);
+
+    @GetMapping(value = "/log", produces = MediaType.TEXT_PLAIN_VALUE)
+    public String log(@RequestParam("msg") String msg) {
+        if (msg != null) {
+            LOG.info("{}", msg);
+        } else {
+            LOG.info("/log called");
+        }
+        return "OK";
+    }
+
+    @PostMapping(value = "/log", produces = MediaType.TEXT_PLAIN_VALUE)
+    public String logPost(@RequestBody String body) {
+        LOG.info("{}", body);
+        return "OK";
+    }
+}
